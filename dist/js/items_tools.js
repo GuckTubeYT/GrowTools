1ea089f6274a091eabc2534df9fe63693557802e
Muhammad Kevin <56192597+GuckTubeYT@users.noreply.github.com>
2023-12-27 20:55:35 +0700

    add warning

--- dist/js/items_tools.js
+++ dist/js/items_tools.js
@@ -553,6 +553,17 @@ function item_decoder(file, using_editor) {
         var version = read_buffer_number(arrayBuffer, 0, 2);
         var item_count = read_buffer_number(arrayBuffer, 2, 4);
 
+        if (version <= 16) {
+            return Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+            }).fire({
+                icon: 'error',
+                title: "Your items.dat version is " + version + ", and This decoder doesnt support that version!"
+            })
+        }
         data_json.version = version
         data_json.item_count = item_count
         data_json.items = []

07b60f6cf6124a953dbd0b4ddc8cae3b129f2b78
Muhammad Kevin <56192597+GuckTubeYT@users.noreply.github.com>
2024-05-23 20:34:57 +0700

    fix break hits encoding

--- dist/js/items_tools.js
+++ dist/js/items_tools.js
@@ -229,7 +229,7 @@ function process_item_encoder(result, using_txt) {
                 
                 // break hits
                 if (result1[16].includes("r")) encoded_buffer_file[mem_pos++] = Number(result1[16].slice(0, -1))
-                else encoded_buffer_file[mem_pos++] = Number(result1[16])
+                else encoded_buffer_file[mem_pos++] = Number(result1[16]) * 6
 
                 // drop chance
                 write_buffer_number(mem_pos, 4, result1[17])
